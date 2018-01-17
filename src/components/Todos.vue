2c4f56defed2fe6295cf2bee99a0fbb8f9177391
DESKTOP-V02D53O\champ <champtnpc@gmail.com>
2018-01-17 19:27:08 +0700

    fix v-else hide strike in Todos.vue #Challenge2 Success

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,8 +3,8 @@
     <div v-for="(todo, index) in todos" :key="todo.title">
       <b-field class="is-pulled-left">
         <b-checkbox size="is-large" @input="setStatusTodo(index)">
-          <strike v-if="todo.completed">{{ todo.title }}</strike >
-          <p v-else="">{{ todo.title }}</p>
+          <strike v-if="todo.completed">{{ todo.title }}</strike>
+          <p v-else="todo.completed">{{ todo.title }}</p>
         </b-checkbox>
       </b-field>
       <a class="delete is-pulled-right" @click="deleteList(index)"></a>
